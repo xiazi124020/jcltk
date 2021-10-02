12ffba16519865a8e0b3535b36590ecc60c0bb7e
xiazi124020 <77074100@qq.com>
2021-10-03 00:17:21 +0900

    download

--- app/views/business.py
+++ app/views/business.py
@@ -13,10 +13,12 @@ from app.util.json import decimal_default_proc, parse_parameters_asjson, parse_p
 from django.db import connection
 import json
 import os
+from os.path import basename
 import urllib, zipfile, shutil, tempfile
 import openpyxl
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+import calendar
 
 logger = logging.getLogger(__name__)
 
@@ -69,11 +71,11 @@ def months_between(start_date, end_date):
     if start_date > end_date:
         raise ValueError(f"Start date {start_date} is not before end date {end_date}")
     
-    start = datetime.strptime(start_date, '%Y%m%d').date()
-    end = datetime.strptime(end_date, '%Y%m%d').date()
+    start = datetime.strptime(start_date[0:6], '%Y%m').date()
+    end = datetime.strptime(end_date[:6], '%Y%m').date()
 
     ret = []
-    while start < end:
+    while start <= end:
         ret.append(datetime.strftime(start, '%Y%m'))
         start += relativedelta(months=1)
     return ret
@@ -105,16 +107,17 @@ def export(request, service="000", start="19000101", end="21991231"):
         on a.project_id = b.id 
     inner join customer c 
         on b.customer_id = c.id 
+       and c.partener = 0
     order by
         c.id
         , c.partener
     """
-    file_datas = []
+    bill_file_datas = []
     with connection.cursor() as cursor:
         cursor.execute(sql)
-        file_datas = dictfetchall(cursor)
+        bill_file_datas = dictfetchall(cursor)
 
-    for data in file_datas:
+    for data in bill_file_datas:
         data['start_date'] = data['start_date'].strftime('%Y%m%d')
         if data['end_date'] is not None:
             data['end_date'] = data['end_date'].strftime('%Y%m%d')
@@ -124,42 +127,70 @@ def export(request, service="000", start="19000101", end="21991231"):
         select emp_id, seisan_time, ym from seisan where ym < %s
     """
     months = months_between(start, end)
-
-    print("=============", months)
+    output_max_month = months[-1]
     base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    # load workbook if you want to write in existing file else use openpyxl.Workbook()
-    wb = openpyxl.load_workbook(os.path.join(base_path, "請求書.xlsx"))
+    
+    zip_tempfile_path = '請求書.zip'
+    download_file_list = []
+    for month in months:
+        customer_id = bill_file_datas[0]['customer_id']
+        # load workbook if you want to write in existing file else use openpyxl.Workbook()
+        wb = openpyxl.load_workbook(os.path.join(base_path, "請求書.xlsx"))
+        filename = "{}_{}_請求書.xlsx"
+        for data in bill_file_datas:
+            count = 0
+            if customer_id != data['customer_id']:
+                customer_id = data['customer_id']
+                # save the excel with name or you can give specific location of your choice
+                filename = filename.format(month, data['customer_name'])
+                wb.save(os.path.join(base_path, filename))
+                wb.close()
+                download_file_list.append(filename)
+                wb = openpyxl.load_workbook(os.path.join(base_path, "請求書.xlsx"))
 
-    #set the sheet name
-    # wb['Sheet'].title="Report of Automation"
+            if month >= data['start_date'][:6] and (data['end_date'] is None or month <= data['end_date'][:6]):
 
-    #get the active sheet
-    sh1=wb.active
-    sh1=wb['請求書']
+                #get the active sheet
+                sh1=wb.active
+                sh1=wb['請求書']
 
-    # pass which row and column and value which you want to update
-    sh1.cell(row=5,column=1,value='Pytest')
-    sh1.cell(row=5,column=2,value='UK')
-    sh1.cell(row=5,column=3,value=88.88)
+                # pass which row and column and value which you want to update
+                sh1.cell(row=2,column=1,value=data['customer_name'] + "御中")
+                sh1.cell(row=2,column=8,value='JCL-' + month + "-" + str(count).zfill(3))
+                sh1.cell(row=3,column=8,value=month[:2] + "/" + month[2:] + "/01")
+                sh1.cell(row=4,column=1,value="〒" + data['customer_zip'][:3] + "-" + data['customer_zip'][3:])
+                sh1.cell(row=8,column=1,value="件名：" + data['project_name'])
+                # 支払期限
+                current_date = datetime.strptime(month + '01', '%Y%m%d')
+                next_two_month = current_date + relativedelta(months=2)
+                next_month = next_two_month.replace(day=28) + timedelta(days=4)
+                last_day_of_month = next_month - timedelta(days=next_month.day)
 
-    # save the excel with name or you can give specific location of your choice
-    wb.save(os.path.join(base_path, "請求書202109.xlsx"))
+                sh1.cell(row=12,column=8,value=datetime.strftime(last_day_of_month, '%Y%m%d'))
+                sh1.cell(row=15+count,column=1,value=count + 1)
+                sh1.cell(row=15+count,column=2,value=data['project_name'] + "【" + data['first_name'] + data['last_name'] +  "】")
+                sh1.cell(row=15+count,column=5,value=1)
+                sh1.cell(row=15+count,column=6,value="人・月")
+                sh1.cell(row=15+count,column=7,value=data['price'])
+                count = count + 1
 
-
-    csv_tempfile_path = os.path.join(tempfile.gettempdir(), f"ユーザー一覧($1-$2).csv")
-    zip_tempfile_path = f'{csv_tempfile_path}.zip'
-
+    with zipfile.ZipFile(os.path.join(base_path,zip_tempfile_path), 'w', compression=zipfile.ZIP_DEFLATED) as new_zip:
+        for f_index in download_file_list:
+            new_zip.write(os.path.join(base_path, f_index))
 
     response = HttpResponse(content_type='application/octet-stream')
-    filename = (f'請求書202109.zip').encode("utf8")
-    # filename = (f'請求書202109.xlsx').encode("utf8")
-    urlenc_filename = urllib.parse.quote(filename)
-    response['Content-Disposition'] = 'attachment;filename="{}";filename*=UTF-8\'\'{}'.format(filename, urlenc_filename)
+    temp_filename = zip_tempfile_path.encode("utf8")
+    urlenc_filename = urllib.parse.quote(temp_filename)
+    response['Content-Disposition'] = 'attachment;filename="{}";filename*=UTF-8\'\'{}'.format(temp_filename, urlenc_filename)
 
-
-    with open(os.path.join(base_path, "請求書202109.zip"), 'rb') as f_in:
+    with open(os.path.join(base_path, zip_tempfile_path), 'rb') as f_in:
         shutil.copyfileobj(f_in, response)
 
+    for f_index in download_file_list:
+        if os.path.exists(os.path.join(base_path, f_index)):
+            os.remove(os.path.join(base_path, f_index))
+    if os.path.exists(os.path.join(base_path,zip_tempfile_path)):
+        os.remove(os.path.join(base_path,zip_tempfile_path))
+
     return response
-    # return HttpResponse(to_json(datas), content_type='application/json')
 
